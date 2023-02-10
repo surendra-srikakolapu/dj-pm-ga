4d12df2a2cc3aa2c0df8df640d5cc82b4f311a51
surendra-srikakolapu <surendra.srikakolapu@gmail.com>
2023-02-10 13:13:47 +0530

    first commit

---
+++ main/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Location
+# Register your models here.
+
+admin.site.site_header = "Personal Password Manager"
+
+
+class AdminConfig(admin.ModelAdmin):
+    list_display = ('website_name', 'author', 'created')
+    exclude = ('website_password', 'master_password',)
+
+admin.site.register(Location, AdminConfig)
